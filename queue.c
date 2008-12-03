e8d3021f57218854bfc4887a6a9e52f7f6d4c781
Gilles Chehade <gilles@poolp.org>
2008-12-03 20:08:08 +0000

    - remove log_debug() that's no longer needed

--- queue.c
+++ queue.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: queue.c,v 1.16 2008/11/25 20:26:40 gilles Exp $	*/
+/*	$OpenBSD: queue.c,v 1.17 2008/12/03 17:58:00 gilles Exp $	*/
 
 /*
  * Copyright (c) 2008 Gilles Chehade <gilles@openbsd.org>
@@ -1315,7 +1315,6 @@ queue_delete_incoming_message(char *message_id)
 			fatal("queue_delete_incoming_message: unlink");
 	}
 
-	log_debug("evpdir: %s", evpdir);
 	dirp = opendir(evpdir);
 	if (dirp == NULL)
 		fatal("queue_delete_incoming_message: opendir");
