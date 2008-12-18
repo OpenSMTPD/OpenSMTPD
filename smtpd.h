de4c17a0eea1b87ec1c0d06ef49f6a9b08a30ab0
jacekm <jacekm>
2008-12-18 23:57:17 +0000

    Introduce safe_fclose, which tries to push file to the disk as quickly as possible; it fails under temporary error conditions, letting caller react appropriately.
    ok gilles@

--- smtpd.h
+++ smtpd.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: smtpd.h,v 1.29 2008/12/18 23:38:12 jacekm Exp $	*/
+/*	$OpenBSD: smtpd.h,v 1.30 2008/12/18 23:49:56 jacekm Exp $	*/
 
 /*
  * Copyright (c) 2008 Gilles Chehade <gilles@openbsd.org>
@@ -766,3 +766,4 @@ struct map	*map_findbyname(struct smtpd *, const char *);
 /* util.c */
 int		bsnprintf(char *, size_t, const char *, ...)
     __attribute__ ((format (printf, 3, 4)));
+int		safe_fclose(FILE *);
