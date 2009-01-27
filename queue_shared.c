af52e0662625e89011b30d132487287704b50540
Gilles Chehade <gilles@poolp.org>
2009-01-27 22:54:01 +0000

    follow the naming convention of other files, discussed with jacekm@

--- queue_shared.c
+++ queue_shared.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: sharedqueue.c,v 1.1 2009/01/26 22:20:31 gilles Exp $	*/
+/*	$OpenBSD: sharedqueue.c,v 1.2 2009/01/27 22:48:29 gilles Exp $	*/
 
 /*
  * Copyright (c) 2008 Gilles Chehade <gilles@openbsd.org>
