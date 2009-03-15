5aa7354ecee2f5c8037d859a78b37fe4b83ce1c3
Gilles Chehade <gilles@poolp.org>
2009-03-15 19:15:25 +0000

    the mda process no longer uses struct batch as its central structure to deal with deliveries, it now uses struct session just like mta and smtp processes. we now keep the mbox and message descriptors in the session, saving space in struct message which is now as small as we can make it. While at it, plugged a memory leak and did some cosmethic changes
    This was the last planned change to our struct message which means that
    later changes will no longer require a queue flush before rebuild.

--- mta.c
+++ mta.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: mta.c,v 1.33 2009/03/10 22:33:26 jacekm Exp $	*/
+/*	$OpenBSD: mta.c,v 1.34 2009/03/12 11:08:26 pea Exp $	*/
 
 /*
  * Copyright (c) 2008 Pierre-Yves Ritschard <pyr@openbsd.org>
@@ -367,7 +367,6 @@ mta_dispatch_runner(int sig, short event, void *p)
 			    sizeof(batchp->session_helo));
 
  			TAILQ_INSERT_TAIL(&batchp->messages, messagep, entry);
-
 			break;
 		}
 		case IMSG_BATCH_CLOSE: {
@@ -1081,4 +1080,5 @@ mta_batch_update_queue(struct batch *batchp)
 		fclose(batchp->messagefp);
 
 	free(batchp);
+
 }
