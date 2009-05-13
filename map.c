95af3edbce838f00ad75b7add9854fa6fd1604b8
jacekm <jacekm>
2009-05-13 21:20:55 +0000

    log dbopen and db->get errors; based on diff from Daniel Ouellet. looks ok to gilles@

--- map.c
+++ map.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: map.c,v 1.4 2009/01/01 16:15:47 jacekm Exp $	*/
+/*	$OpenBSD: map.c,v 1.5 2009/03/06 23:38:18 gilles Exp $	*/
 
 /*
  * Copyright (c) 2008 Gilles Chehade <gilles@openbsd.org>
@@ -76,13 +76,16 @@ map_dblookup(struct smtpd *env, char *mapname, char *keyname)
 	}
 
 	db = dbopen(map->m_config, O_RDONLY, 0600, DB_HASH, NULL);
-	if (db == NULL)
+	if (db == NULL) {
+		log_warn("map_dblookup: can't open %s", map->m_config);
 		return NULL;
+	}
 
 	key.data = keyname;
 	key.size = strlen(key.data) + 1;
 
 	if ((ret = db->get(db, &key, &val, 0)) == -1) {
+		log_warn("map_dblookup: map '%s'", mapname);
 		db->close(db);
 		return NULL;
 	}
