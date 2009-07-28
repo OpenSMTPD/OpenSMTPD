71660a942200c0ebeff4d1b2c4dcc78211dd5264
Gilles Chehade <gilles@poolp.org>
2009-07-28 13:54:35 +0000

    indent

--- smtpd.c
+++ smtpd.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: smtpd.c,v 1.74 2009/06/05 20:43:57 pyr Exp $	*/
+/*	$OpenBSD: smtpd.c,v 1.75 2009/06/06 04:14:21 pyr Exp $	*/
 
 /*
  * Copyright (c) 2008 Gilles Chehade <gilles@openbsd.org>
@@ -1226,7 +1226,8 @@ parent_mailbox_open(char *path, struct passwd *pw, struct batch *batchp)
 		close(STDERR_FILENO);
 		dup2(pipefd[1], 0);
 
-		execlp(PATH_MAILLOCAL, "mail.local", "-f", sender, pw->pw_name, (void *)NULL);
+		execlp(PATH_MAILLOCAL, "mail.local", "-f", sender, pw->pw_name,
+		    (void *)NULL);
 		_exit(1);
 	}
 
