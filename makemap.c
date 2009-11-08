88b918b46fd5ba60be36ad86d3fa433fd21e3a07
Gilles Chehade <gilles@poolp.org>
2009-11-08 23:08:56 +0000

    rework a bit expansion and data structures involved in the expansion so we no longer have a direct mapping between structures saved in aliases/virtual db and structures used at runtime during expansion.
    side effects ? struct alias is smaller, databases are smaller and it is no
    longer necessary to rebuild aliases/virtual databases each time jacekm@ or
    I make changes to some obscure structure used indirectely during expansion
    
    rebuild databases, flush queues, make clean

--- makemap.c
+++ makemap.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: makemap.c,v 1.22 2009/10/17 12:46:47 sobrado Exp $	*/
+/*	$OpenBSD: makemap.c,v 1.23 2009/11/03 22:57:41 gilles Exp $	*/
 
 /*
  * Copyright (c) 2008 Gilles Chehade <gilles@openbsd.org>
@@ -354,9 +354,9 @@ make_plain(DBT *val, char *text)
 int
 make_aliases(DBT *val, char *text)
 {
-	struct alias	 a;
-	char		*subrcpt;
-	char		*endp;
+	struct alias	a;
+	char	       	*subrcpt;
+	char	       	*endp;
 
 	val->data = NULL;
 	val->size = 0;
