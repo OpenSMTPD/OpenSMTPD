47a100ed117ecfd6a6f590b0b453efb1519762de
Charles Longeau <github@chl.be>
2009-11-14 18:48:05 +0000

    remove duplicate header include
    ok gilles@

--- smtpd.c
+++ smtpd.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: smtpd.c,v 1.87 2009/10/19 20:00:46 gilles Exp $	*/
+/*	$OpenBSD: smtpd.c,v 1.88 2009/11/03 11:10:43 jacekm Exp $	*/
 
 /*
  * Copyright (c) 2008 Gilles Chehade <gilles@openbsd.org>
@@ -34,7 +34,6 @@
 #include <event.h>
 #include <fcntl.h>
 #include <login_cap.h>
-#include <paths.h>
 #include <paths.h>
 #include <pwd.h>
 #include <regex.h>
