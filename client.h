80cb7dde57f5a9e3d3fd8d0a15cd345bece66f35
jacekm <jacekm>
2010-01-02 11:06:37 +0000

    Simplify code by removing struct smtp_client member, int iomode, which was used for keeping track of the current polling mode.  Introduce new function client_poll() that determines what mode of polling is required at the time of call.

--- client.h
+++ client.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: client.h,v 1.7 2009/12/15 11:45:51 jacekm Exp $	*/
+/*	$OpenBSD: client.h,v 1.8 2009/12/23 17:16:03 jacekm Exp $	*/
 
 /*
  * Copyright (c) 2009 Jacek Masiulaniec <jacekm@dobremiasto.net>
@@ -85,7 +85,6 @@ struct smtp_client {
 	struct msgbuf		 w;
 	short			 ssl_handshake;
 	void			*ssl;
-	int			 iomode;
 	int			 sndlowat;
 	struct timeval		 timeout;
 	FILE			*verbose;
