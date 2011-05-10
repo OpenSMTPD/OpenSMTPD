52c27a830608627ed7d89d27f0a8bb584ef93350
Gilles Chehade <gilles@poolp.org>
2011-05-10 17:04:43 +0000

    fix an off-by-one that made smtpd skip an "invalid" bucket that was actually valid.
    bugfix tested by todd@

--- queue_shared.c
+++ queue_shared.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: queue_shared.c,v 1.46 2011/04/17 11:39:22 gilles Exp $	*/
+/*	$OpenBSD: queue_shared.c,v 1.47 2011/05/01 12:57:11 eric Exp $	*/
 
 /*
  * Copyright (c) 2008 Gilles Chehade <gilles@openbsd.org>
@@ -223,7 +223,7 @@ walk_queue(struct qwalk *q, char *fname)
 			log_warnx("walk_queue: invalid bucket: %s", fname);
 			return (QWALK_AGAIN);
 		}
-		if (errno == ERANGE || q->bucket >= DIRHASH_BUCKETS - 1) {
+		if (errno == ERANGE || q->bucket >= DIRHASH_BUCKETS) {
 			log_warnx("walk_queue: invalid bucket: %s", fname);
 			return (QWALK_AGAIN);
 		}
