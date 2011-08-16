d10e41406288e92fa07f0604a63568f8ac28f998
Gilles Chehade <gilles@poolp.org>
2011-08-16 19:12:40 +0000

    smtpctl show sizes, displays the size of queue-related structures, useful for developers to see the impact of structure changes on memory and disk usage, and useful for users to better understand 'smtpctl show stats'

--- parser.h
+++ parser.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: parser.h,v 1.16 2011/04/13 20:53:18 gilles Exp $	*/
+/*	$OpenBSD: parser.h,v 1.17 2011/07/21 23:29:24 gilles Exp $	*/
 
 /*
  * Copyright (c) 2006 Pierre-Yves Ritschard <pyr@openbsd.org>
@@ -27,6 +27,7 @@ enum actions {
 	SHOW_QUEUE,
 	SHOW_RUNQUEUE,
 	SHOW_STATS,
+	SHOW_SIZES,
 	PAUSE_MDA,
 	PAUSE_MTA,
 	PAUSE_SMTP,
