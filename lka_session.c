02d138b521c2fc1f8c290107c683331a666b58c0
Gilles Chehade <gilles@poolp.org>
2011-08-27 22:32:42 +0000

    initial support for a session-time filtering API
    currently only HELO/EHLO, MAIL, RCPT are supported, however ... I have
    voluntarily disabled filters at smtpd.conf level so people don't play with
    it until the API has stabilized a bit
    
    discussed with several people in private, no one opposed the feature

--- lka_session.c
+++ lka_session.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: lka_session.c,v 1.7 2011/06/09 17:41:52 gilles Exp $	*/
+/*	$OpenBSD: lka_session.c,v 1.8 2011/07/04 19:44:31 gilles Exp $	*/
 
 /*
  * Copyright (c) 2011 Gilles Chehade <gilles@openbsd.org>
@@ -650,6 +650,5 @@ lka_session_cmp(struct lka_session *s1, struct lka_session *s2)
 
 	return 0;
 }
-
 
 SPLAY_GENERATE(lkatree, lka_session, nodes, lka_session_cmp);
