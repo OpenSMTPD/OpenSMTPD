904cb25ab9337fd86f88472134c273089cfc1031
Eric Faurot <eric@faurot.net>
2011-10-23 17:09:56 +0000

    plug leak
    spotted by chl@
    
    ok chl@

--- mta.c
+++ mta.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: mta.c,v 1.115 2011/10/23 09:30:07 gilles Exp $	*/
+/*	$OpenBSD: mta.c,v 1.116 2011/10/23 15:36:53 eric Exp $	*/
 
 /*
  * Copyright (c) 2008 Pierre-Yves Ritschard <pyr@openbsd.org>
@@ -520,6 +520,7 @@ mta_enter_state(struct mta_session *s, int newstate, void *p)
 		if (s->datafp)
 			fclose(s->datafp);
 
+		free(s->authmap);
 		free(s->secret);
 		free(s->host);
 		free(s->cert);
