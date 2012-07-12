e8c16139fa5e3eee13e3af1bfa95e93fb1652d87
Charles Longeau <github@chl.be>
2012-07-12 08:51:43 +0000

    add support for maildir tagging/folders.
    ok gilles@
    ok eric@ on previous versions of this patch

--- delivery_maildir.c
+++ delivery_maildir.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: delivery_maildir.c,v 1.5 2012/03/07 23:04:00 gilles Exp $	*/
+/*	$OpenBSD: delivery_maildir.c,v 1.6 2012/05/25 13:51:42 chl Exp $	*/
 
 /*
  * Copyright (c) 2011 Gilles Chehade <gilles@openbsd.org>
@@ -62,7 +62,7 @@ delivery_maildir_open(struct deliver *deliver)
 #define error2(m)	{ msg = m; goto err2; }
 
 	setproctitle("maildir delivery");
-	if (mkdir(deliver->to, 0700) < 0 && errno != EEXIST)
+	if (mkdir_p(deliver->to, 0700) < 0 && errno != EEXIST)
 		error("cannot mkdir maildir");
 	if (chdir(deliver->to) < 0)
 		error("cannot cd to maildir");
