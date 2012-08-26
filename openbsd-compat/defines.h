5d457b758d4ef5cf82404883cf882a9a74e16a9b
Charles Longeau <github@chl.be>
2012-08-26 02:40:29 +0200

    add timespecsub for systems which doesn't have it

--- openbsd-compat/defines.h
+++ openbsd-compat/defines.h
@@ -428,6 +428,18 @@ struct winsize {
    } while (0)
 #endif
 
+#ifndef timespecsub
+#define timespecsub(a, b, result)				\
+   do {								\
+      (result)->tv_sec = (a)->tv_sec - (b)->tv_sec;		\
+      (result)->tv_nsec = (a)->tv_nsec - (b)->tv_nsec;		\
+      if ((result)->tv_nsec < 0) {				\
+	 --(result)->tv_sec;					\
+	 (result)->tv_nsec += 1000000000L;			\
+      }								\
+   } while (0)
+#endif
+
 #ifndef TIMEVAL_TO_TIMESPEC
 #define	TIMEVAL_TO_TIMESPEC(tv, ts) {					\
 	(ts)->tv_sec = (tv)->tv_sec;					\
