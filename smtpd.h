635e46461fa507dc3be32604b3673c70af238127
Eric Faurot <eric@faurot.net>
2012-10-28 08:46:26 +0000

    Limit the number of messages that can be enqueued on a single SMTP connection, and the number of recipients in each of them.
    ok gilles@ chl@

--- smtpd.h
+++ smtpd.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: smtpd.h,v 1.389 2012/10/14 11:58:23 gilles Exp $	*/
+/*	$OpenBSD: smtpd.h,v 1.390 2012/10/16 12:02:23 eric Exp $	*/
 
 /*
  * Copyright (c) 2008 Gilles Chehade <gilles@openbsd.org>
@@ -543,6 +543,7 @@ struct session {
 	struct timeval			 s_tv;
 	struct envelope			 s_msg;
 	short				 s_nresp[STATE_COUNT];
+	size_t				 mailcount;
 	size_t				 rcptcount;
 	long				 s_datalen;
 
