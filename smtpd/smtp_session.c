d8d351c24b2fa6352559f65a657e4b727cbab77d
Charles Longeau <github@chl.be>
2012-10-29 23:42:55 +0100

    remove evbuffer_readln_crlf(), reported by Eric Faurot

--- smtpd/smtp_session.c
+++ smtpd/smtp_session.c
@@ -119,40 +119,6 @@ struct session_cmd rfc4954_cmdtab[] = {
 	{ "auth",	session_rfc4954_auth_handler }
 };
 
-#ifndef EVBUFFER_EOL_CRLF
-char *
-evbuffer_readln_crlf(struct evbuffer *buffer)
-{
-        u_char *data = EVBUFFER_DATA(buffer);
-        size_t len = EVBUFFER_LENGTH(buffer);
-        char *line;
-        unsigned int i, j;
-
-        for (i = 0; i < len; ++i) {
-                if (data[i] == '\n')
-                        break;
-        }
-
-        if (i == len)
-                return NULL;
-
-        j = i;
-        if (i != 0 && data[i - 1] == '\r')
-                --j;
-
-        line = calloc(j + 1, 1);
-        if (line == NULL)
-                fatal("calloc");
-
-        if (j != 0)
-                memcpy(line, data, j);
-
-        evbuffer_drain(buffer, i + 1);
-
-        return (line);
-}
-#endif
-
 static int
 session_rfc3207_stls_handler(struct session *s, char *args)
 {
