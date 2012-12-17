133dc3633452901701dffb7a467d08069ffd5010
Eric Faurot <eric@faurot.net>
2012-12-17 16:50:05 +0100

    fix includes

--- smtpd/mproc.c
+++ smtpd/mproc.c
@@ -16,10 +16,12 @@
  * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  */
 
+#include "includes.h"
+
 #include <sys/types.h>
 #include <sys/socket.h>
-#include <sys/tree.h>
-#include <sys/queue.h>
+#include <sys-tree.h>
+#include <sys-queue.h>
 #include <sys/uio.h>
 
 #include <netinet/in.h>
