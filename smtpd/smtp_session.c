77ea91b85c9c3f259908fc7f581d3966440f2525
Gilles Chehade <gilles@poolp.org>
2013-03-15 12:44:15 +0100

    typo in trace message

--- smtpd/smtp_session.c
+++ smtpd/smtp_session.c
@@ -1594,7 +1594,7 @@ smtp_auth_failure_pause(struct smtp_session *s)
 	tv.tv_sec = 0;
 	tv.tv_usec = arc4random_uniform(1000000);
 	log_trace(TRACE_SMTP, "smtp: timing-attack protection triggered, "
-	    "will defer answer for for %lu microseconds", tv.tv_usec);
+	    "will defer answer for %lu microseconds", tv.tv_usec);
 	evtimer_set(&s->pause, smtp_auth_failure_resume, s);
 	evtimer_add(&s->pause, &tv);
 }
