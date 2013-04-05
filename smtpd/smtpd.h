6ec2a525d6385e78121b3b019d1784903970d9e6
gilles <gilles@poolp.org>
2013-04-05 11:17:41 +0200

    woops was in wrong branch

--- smtpd/smtpd.h
+++ smtpd/smtpd.h
@@ -82,7 +82,7 @@
 #ifndef SMTPD_NAME
 #define	SMTPD_NAME		 "OpenSMTPD"
 #endif
-#define	SMTPD_VERSION		 "5.3.1p1"
+#define	SMTPD_VERSION		 "5.3p1"
 #define SMTPD_BANNER		 "220 %s ESMTP %s"
 #define SMTPD_SESSION_TIMEOUT	 300
 #define SMTPD_BACKLOG		 5
