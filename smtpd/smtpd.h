c2437efd66ed4c33aeea77dde9541e528577453d
Eric Faurot <eric@faurot.net>
2013-05-30 11:19:29 +0200

    5.3.2 -> 5.3.3

--- smtpd/smtpd.h
+++ smtpd/smtpd.h
@@ -55,7 +55,7 @@
 #ifndef SMTPD_NAME
 #define	SMTPD_NAME		 "OpenSMTPD"
 #endif
-#define	SMTPD_VERSION		 "5.3.2"
+#define	SMTPD_VERSION		 "5.3.3"
 #define SMTPD_BANNER		 "220 %s ESMTP %s"
 #define SMTPD_SESSION_TIMEOUT	 300
 #define SMTPD_BACKLOG		 5
