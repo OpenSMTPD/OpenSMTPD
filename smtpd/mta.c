cb24a838fe6b0e95654c5b38060b755e80049e29
Eric Faurot <eric@faurot.net>
2013-06-03 17:25:15 +0200

    fix memleaks

--- smtpd/mta.c
+++ smtpd/mta.c
@@ -1421,6 +1421,7 @@ mta_host_unref(struct mta_host *h)
 	SPLAY_REMOVE(mta_host_tree, &hosts, h);
 	free(h->sa);
 	free(h->ptrname);
+	free(h);
 	stat_decrement("mta.host", 1);
 }
 
@@ -1496,6 +1497,7 @@ mta_domain_unref(struct mta_domain *d)
 
 	SPLAY_REMOVE(mta_domain_tree, &domains, d);
 	free(d->name);
+	free(d);
 	stat_decrement("mta.domain", 1);
 }
 
@@ -1550,6 +1552,7 @@ mta_source_unref(struct mta_source *s)
 
 	SPLAY_REMOVE(mta_source_tree, &sources, s);
 	free(s->sa);
+	free(s);
 	stat_decrement("mta.source", 1);
 }
 
@@ -1609,6 +1612,7 @@ mta_connector_free(struct mta_connector *c)
 	c->relay->nconnector--;
 	TAILQ_REMOVE(c->queue, c, lst_entry);
 	mta_source_unref(c->source);
+	feee(c);
 	stat_decrement("mta.connector", 1);
 }
 
@@ -1663,6 +1667,7 @@ mta_route_unref(struct mta_route *r)
 	SPLAY_REMOVE(mta_route_tree, &routes, r);
 	mta_source_unref(r->src); /* from constructor */
 	mta_host_unref(r->dst); /* from constructor */
+	free(r);
 	stat_decrement("mta.route", 1);
 }
 
