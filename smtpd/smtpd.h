b383faed22a91e9d123465e44a743fe06e9f0086
Eric Faurot <eric@faurot.net>
2013-06-06 12:02:43 +0200

    use branch name as version

--- smtpd/smtpd.h
+++ smtpd/smtpd.h
@@ -74,7 +74,7 @@
 #ifndef SMTPD_NAME
 #define	SMTPD_NAME		 "OpenSMTPD"
 #endif
-#define	SMTPD_VERSION		 "5.3p1"
+#define	SMTPD_VERSION		 "portable"
 #define SMTPD_BANNER		 "220 %s ESMTP %s"
 #define SMTPD_SESSION_TIMEOUT	 300
 #define SMTPD_BACKLOG		 5
