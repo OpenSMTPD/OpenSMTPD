0ab0996c999bd23986d9512ef2648fe27f3bc4fd
Eric Faurot <eric@faurot.net>
2013-06-20 15:55:16 +0200

    Reorganize code as for queue_*.c

--- smtpd/table_api.c
+++ smtpd/table_api.c
@@ -37,131 +37,165 @@ static int (*handler_check)(int, const char *);
 static int (*handler_lookup)(int, const char *, char *, size_t);
 static int (*handler_fetch)(int, char *, size_t);
 
-static struct imsgbuf	ibuf;
-static struct imsg	imsg;
+static int		 quit;
+static struct imsgbuf	 ibuf;
+static struct imsg	 imsg;
+static size_t		 rlen;
+static char		*rdata;
+static struct ibuf	*buf;
 
-static int
-dispatch(void)
+static void
+table_msg_get(void *dst, size_t len)
+{
+	if (len > rlen) {
+		log_warnx("warn: table-proc: bad msg len");
+		fatalx("table-proc: exiting");
+	}
+
+	if (len == 0)
+		return;
+
+	if (dst)
+		memmove(dst, rdata, len);
+
+	rlen -= len;
+	rdata += len;
+}
+
+static void
+table_msg_end(void)
+{
+	if (rlen) {
+		log_warnx("warn: table-proc: bogus data");
+		fatalx("table-proc: exiting");
+	}
+	imsg_free(&imsg);
+}
+
+static void
+table_msg_add(const void *data, size_t len)
+{
+	if (buf == NULL)
+		buf = imsg_create(&ibuf, PROC_TABLE_OK, 0, 0, 1024);
+	if (buf == NULL) {
+		log_warnx("warn: table-api: imsg_create failed");
+		fatalx("table-api: exiting");
+	}
+	if (imsg_add(buf, data, len) == -1) {
+		log_warnx("warn: table-api: imsg_add failed");
+		fatalx("table-api: exiting");
+	}
+}
+
+static void
+table_msg_close(void)
+{
+	imsg_close(&ibuf, buf);
+	buf = NULL;
+}
+
+static void
+table_msg_dispatch(void)
 {
 	uint32_t	 version;
-	size_t		 len;
-	char		 res[4096], *data, *key;
+	char		 res[4096];
 	int		 type, r;
-	struct ibuf	*buf;
-
-	data = imsg.data;
-	len = imsg.hdr.len - IMSG_HEADER_SIZE;
 
 	switch (imsg.hdr.type) {
 	case PROC_TABLE_OPEN:
-		if (len != sizeof(version)) {
-			log_warnx("warn: table-api: bad message length");
-			return (-1);
-		}
-		memmove(&version, data, len);
+		table_msg_get(&version, sizeof(version));
+		table_msg_end();
+
 		if (version != PROC_TABLE_API_VERSION) {
 			log_warnx("warn: table-api: bad API version");
-			return (-1);
+			fatalx("table-api: terminating");
 		}
+
 		imsg_compose(&ibuf, PROC_TABLE_OK, 0, 0, -1, NULL, 0);
-		return (0);
+		break;
 
 	case PROC_TABLE_UPDATE:
-		if (len != 0) {
-			log_warnx("warn: table-api: bad message length");
-			return (-1);
-		}
+		table_msg_end();
+
 		if (handler_update)
 			r = handler_update();
 		else
 			r = 1;
+
 		imsg_compose(&ibuf, PROC_TABLE_OK, 0, 0, -1, &r, sizeof(r));
-		return (0);
+		break;
 
 	case PROC_TABLE_CLOSE:
-		return (-1);
+		quit = 1;
+		break;
 
 	case PROC_TABLE_CHECK:
-		if (len <= sizeof (type)) {
-			log_warnx("warn: table-api: bad message length");
-			return (-1);
+		table_msg_get(&type, sizeof(type));
+		if (rlen == 0) {
+			log_warnx("warn: table-api: no key");
+			fatalx("table-api: exiting");
 		}
-		memmove(&type, data, sizeof(type));
-		key = data + sizeof(type);
-		len -= sizeof(type);
-		if (key[len - 1] != '\0') {
-			log_warnx("warn: table-api: bad message length");
-			return (-1);
+		if (rdata[rlen - 1] != '\0') {
+			log_warnx("warn: table-api: key not NUL-terminated");
+			fatalx("table-api: exiting");
 		}
 
 		if (handler_check)
-			r = handler_check(type, key);
+			r = handler_check(type, rdata);
 		else
 			r = -1;
+		table_msg_get(NULL, rlen);
+		table_msg_end();
 
-		imsg_compose(&ibuf, PROC_TABLE_OK, 0, 0, -1, &r, sizeof(r));
-		return (0);
+		table_msg_add(&r, sizeof(r));
+		table_msg_close();
+		break;
 
 	case PROC_TABLE_LOOKUP:
-		if (len <= sizeof (type)) {
-			log_warnx("warn: table-api: bad message length");
-			return (-1);
+		table_msg_get(&type, sizeof(type));
+
+		if (rlen == 0) {
+			log_warnx("warn: table-api: no key");
+			fatalx("table-api: exiting");
 		}
-		memmove(&type, data, sizeof(type));
-		key = data + sizeof(type);
-		len -= sizeof(type);
-		if (key[len - 1] != '\0') {
-			log_warnx("warn: table-api: bad message length");
-			return (-1);
+		if (rdata[rlen - 1] != '\0') {
+			log_warnx("warn: table-api: key not NUL-terminated");
+			fatalx("table-api: exiting");
 		}
 
 		if (handler_lookup)
-			r = handler_lookup(type, key, res, sizeof(res));
+			r = handler_lookup(type, rdata, res, sizeof(res));
 		else
 			r = -1;
 
-		len = sizeof(r);
+		table_msg_get(NULL, rlen);
+		table_msg_end();
+
+		table_msg_add(&r, sizeof(r));
 		if (r == 1)
-			len += strlen(res) + 1;
-		buf = imsg_create(&ibuf, PROC_TABLE_OK, 0, 0, len);
-		if (imsg_add(buf, &r, sizeof(r)) == -1) {
-			log_warnx("warn: table-api: imsg_add failure");
-			return (-1);
-		}
-		if (r == 1)
-			if (imsg_add(buf, res, strlen(res) + 1) == -1) {
-				log_warnx("warn: table-api: imsg_add failure");
-				return (-1);
-			}
-		imsg_close(&ibuf, buf);
-		return (0);
+			table_msg_add(res, strlen(res) + 1);
+		table_msg_close();
+		break;
+
 
 	case PROC_TABLE_FETCH:
-		if (len != sizeof(type)) {
-			log_warnx("warn: table-api: bad message length");
-			return (-1);
-		}
-		memmove(&type, data, sizeof(type));
+		table_msg_get(&type, sizeof(type));
+		table_msg_end();
 
 		if (handler_fetch)
 			r = handler_fetch(type, res, sizeof(res));
 		else
 			r = -1;
-		len = sizeof(r);
+
+		table_msg_add(&r, sizeof(r));
 		if (r == 1)
-			len += strlen(res) + 1;
-		buf = imsg_create(&ibuf, PROC_TABLE_OK, 0, 0, len);
-		if (imsg_add(buf, &r, sizeof(r)) == -1)
-			return (-1);
-		if (r == 1)
-			if (imsg_add(buf, res, strlen(res) + 1) == -1)
-				return (-1);
-		imsg_close(&ibuf, buf);
-		return (0);
+			table_msg_add(res, strlen(res) + 1);
+		table_msg_close();
+		break;
 
 	default:
 		log_warnx("warn: table-api: bad message %i", imsg.hdr.type);
-		return (-1);
+		fatalx("table-api: exiting");
 	}
 }
 
@@ -193,7 +227,6 @@ int
 table_api_dispatch(void)
 {
 	ssize_t	n;
-	int	r;
 
 	imsg_init(&ibuf, 0);
 
@@ -205,9 +238,10 @@ table_api_dispatch(void)
 		}
 
 		if (n) {
-			r = dispatch();
-			imsg_free(&imsg);
-			if (r == -1)
+			rdata = imsg.data;
+			rlen = imsg.hdr.len - IMSG_HEADER_SIZE;
+			table_msg_dispatch();
+			if (quit)
 				break;
 			imsg_flush(&ibuf);
 			continue;
@@ -224,5 +258,5 @@ table_api_dispatch(void)
 		}
 	}
 
-	return (0);
+	return (1);
 }
