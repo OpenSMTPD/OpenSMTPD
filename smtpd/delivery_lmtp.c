641be52ae8da442ea899cb3740acd5933542b959
Eric Faurot <eric@faurot.net>
2013-07-19 10:41:58 +0200

    Error out if the state is invalid.

--- smtpd/delivery_lmtp.c
+++ smtpd/delivery_lmtp.c
@@ -215,8 +215,9 @@ delivery_lmtp_open(struct deliver *deliver)
 			 fprintf(fp, "QUIT\r\n");
 			 state = LMTP_BYE;
 			 break;
+
 		 default:
-			 _exit(0);
+			errx(1, "Bogus state %i", state);
 		 }
 	 }
 
