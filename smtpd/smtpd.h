5474a644f064b8dadb835a16e3ade82051ebe507
Eric Faurot <eric@faurot.net>
2013-07-22 15:53:05 +0200

    set version to 5.4

--- smtpd/smtpd.h
+++ smtpd/smtpd.h
@@ -52,7 +52,7 @@
 #ifndef SMTPD_NAME
 #define	SMTPD_NAME		 "OpenSMTPD"
 #endif
-#define	SMTPD_VERSION		 "master"
+#define	SMTPD_VERSION		 "5.4"
 #define SMTPD_BANNER		 "220 %s ESMTP %s"
 #define SMTPD_SESSION_TIMEOUT	 300
 #define SMTPD_BACKLOG		 5
