e4397d5fac451b24acacaf850a1438252160a21e
Eric Faurot <eric@faurot.net>
2013-08-21 11:09:48 +0200

    do not account for mta session errors on MXs, but on the routes. If a route has too many of these errors, disable it for a while. reset the error counter for a route when it is re-enabled or when it could establish a connection successfully.

--- smtpd/smtpd.h
+++ smtpd/smtpd.h
@@ -603,7 +603,6 @@ struct mta_host {
 
 #define HOST_IGNORE	0x01
 	int			 flags;
-	int			 nerror;
 };
 
 struct mta_mx {
@@ -673,6 +672,7 @@ struct mta_route {
 #define ROUTE_DISABLED_NET	0x10
 #define ROUTE_DISABLED_SMTP	0x20
 	int			 flags;
+	int			 nerror;
 	int			 penalty;
 	int			 refcount;
 	size_t			 nconn;
