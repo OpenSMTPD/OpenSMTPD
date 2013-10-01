bf9762776d95f89bb1462d8be6f2cba03ed905fc
Gilles Chehade <gilles@poolp.org>
2013-10-01 13:17:06 +0200

    replace all arc4* calls to csprng_* calls, currently mapped to arc4

--- smtpd/smtpd.h
+++ smtpd/smtpd.h
@@ -1393,7 +1393,9 @@ void session_socket_blockmode(int, enum blockmodes);
 void session_socket_no_linger(int);
 int session_socket_error(int);
 int getmailname(char *, size_t);
-
+uint32_t csprng_random(void);
+void csprng_random_buf(void *, size_t);
+uint32_t csprng_random_uniform(uint32_t);
 
 /* waitq.c */
 int  waitq_wait(void *, void (*)(void *, void *, void *), void *);
