9cd3a872cd8cb2fd183f93cd9ed74cebfabdc58c
Gilles Chehade <gilles@poolp.org>
2013-10-01 18:22:20 +0200

    introduce csprng.c instead of having to pull util.c from everywhere

--- smtpd/util.c
+++ smtpd/util.c
@@ -792,20 +792,3 @@ end:
         return ret;
 }
 
-uint32_t
-csprng_random(void)
-{
-	return arc4random();
-}
-
-void
-csprng_buffer(void *buf, size_t nbytes)
-{
-	arc4random_buf(buf, nbytes);
-}
-
-uint32_t
-csprng_uniform(uint32_t upper_bound)
-{
-	return arc4random_uniform(upper_bound);
-}
