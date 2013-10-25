f9e066f948738b676e274257d0756f9e81cf1897
Eric Faurot <eric@faurot.net>
2013-10-25 14:00:12 +0200

    fix formating

--- smtpd/envelope_v1.c
+++ smtpd/envelope_v1.c
@@ -60,14 +60,15 @@ envelope_load_buffer_v1(struct envelope *ep, struct dict *d)
 			goto err;
 
 	/* Transition for old envelopes */
-        if (ep->smtpname[0] == 0)
-                strlcpy(ep->smtpname, env->sc_hostname, sizeof(ep->smtpname));
+	if (ep->smtpname[0] == 0)
+		strlcpy(ep->smtpname, env->sc_hostname, sizeof(ep->smtpname));
 
 	return (1);
 
 err:
 	return (0);
 }
+
 
 static int
 envelope_ascii_load_v1(const char *field, struct envelope *ep, char *buf)
@@ -76,97 +77,97 @@ envelope_ascii_load_v1(const char *field, struct envelope *ep, char *buf)
 		return envelope_ascii_load_uint32(&ep->version, buf);
 
 	if (strcasecmp("tag", field) == 0)
-                return envelope_ascii_load_string(ep->tag, buf, sizeof ep->tag);
+		return envelope_ascii_load_string(ep->tag, buf, sizeof ep->tag);
 
 	if (strcasecmp("msgid", field) == 0)
-                return 1;
+		return 1;
 
 	if (strcasecmp("type", field) == 0)
-                return envelope_ascii_load_type(&ep->type, buf);
+		return envelope_ascii_load_type(&ep->type, buf);
 
 	if (strcasecmp("helo", field) == 0)
-                return envelope_ascii_load_string(ep->helo, buf, sizeof ep->helo);
+		return envelope_ascii_load_string(ep->helo, buf, sizeof ep->helo);
 
-	if (strcasecmp("hostname", field)  == 0)
-                return envelope_ascii_load_string(ep->hostname, buf,
+	if (strcasecmp("hostname", field) == 0)
+		return envelope_ascii_load_string(ep->hostname, buf,
 		    sizeof ep->hostname);
 
-	if (strcasecmp("errorline", field)  == 0)
-                return envelope_ascii_load_string(ep->errorline, buf,
-                    sizeof ep->errorline);
+	if (strcasecmp("errorline", field) == 0)
+		return envelope_ascii_load_string(ep->errorline, buf,
+		    sizeof ep->errorline);
 
-	if (strcasecmp("sockaddr", field)  == 0)
-                return envelope_ascii_load_sockaddr(&ep->ss, buf);
+	if (strcasecmp("sockaddr", field) == 0)
+		return envelope_ascii_load_sockaddr(&ep->ss, buf);
 
-	if (strcasecmp("sender", field)  == 0)
-                return envelope_ascii_load_mailaddr(&ep->sender, buf);
+	if (strcasecmp("sender", field) == 0)
+		return envelope_ascii_load_mailaddr(&ep->sender, buf);
 
-	if (strcasecmp("rcpt", field)  == 0)
-                return envelope_ascii_load_mailaddr(&ep->rcpt, buf);
+	if (strcasecmp("rcpt", field) == 0)
+		return envelope_ascii_load_mailaddr(&ep->rcpt, buf);
 
-	if (strcasecmp("dest", field)  == 0)
-                return envelope_ascii_load_mailaddr(&ep->dest, buf);
+	if (strcasecmp("dest", field) == 0)
+		return envelope_ascii_load_mailaddr(&ep->dest, buf);
 
-	if (strcasecmp("mda-method", field)  == 0)
-                return envelope_ascii_load_mda_method(&ep->agent.mda.method, buf);
+	if (strcasecmp("mda-method", field) == 0)
+		return envelope_ascii_load_mda_method(&ep->agent.mda.method, buf);
 
-	if (strcasecmp("mda-buffer", field)  == 0)
-                return envelope_ascii_load_string(ep->agent.mda.buffer, buf,
-                    sizeof ep->agent.mda.buffer);
+	if (strcasecmp("mda-buffer", field) == 0)
+		return envelope_ascii_load_string(ep->agent.mda.buffer, buf,
+		    sizeof ep->agent.mda.buffer);
 
-	if (strcasecmp("mda-user", field)  == 0)
-                return envelope_ascii_load_string(ep->agent.mda.username, buf,
-                    sizeof ep->agent.mda.username);
+	if (strcasecmp("mda-user", field) == 0)
+		return envelope_ascii_load_string(ep->agent.mda.username, buf,
+		    sizeof ep->agent.mda.username);
 
-	if (strcasecmp("mda-usertable", field)  == 0)
-                return envelope_ascii_load_string(ep->agent.mda.usertable, buf,
-                    sizeof ep->agent.mda.usertable);
+	if (strcasecmp("mda-usertable", field) == 0)
+		return envelope_ascii_load_string(ep->agent.mda.usertable, buf,
+		    sizeof ep->agent.mda.usertable);
 
-	if (strcasecmp("mta-relay-source", field)  == 0)
-                return envelope_ascii_load_string(ep->agent.mta.relay.sourcetable, buf,
-                    sizeof ep->agent.mta.relay.sourcetable);
+	if (strcasecmp("mta-relay-source", field) == 0)
+		return envelope_ascii_load_string(ep->agent.mta.relay.sourcetable, buf,
+		    sizeof ep->agent.mta.relay.sourcetable);
 
-	if (strcasecmp("mta-relay-cert", field)  == 0)
-                return envelope_ascii_load_string(ep->agent.mta.relay.cert, buf,
-                    sizeof ep->agent.mta.relay.cert);
+	if (strcasecmp("mta-relay-cert", field) == 0)
+		return envelope_ascii_load_string(ep->agent.mta.relay.cert, buf,
+		    sizeof ep->agent.mta.relay.cert);
 
-	if (strcasecmp("mta-relay-auth", field)  == 0)
-                return envelope_ascii_load_string(ep->agent.mta.relay.authtable, buf,
-                    sizeof ep->agent.mta.relay.authtable);
+	if (strcasecmp("mta-relay-auth", field) == 0)
+		return envelope_ascii_load_string(ep->agent.mta.relay.authtable, buf,
+		    sizeof ep->agent.mta.relay.authtable);
 
-	if (strcasecmp("mta-relay-helo", field)  == 0)
-                return envelope_ascii_load_string(ep->agent.mta.relay.helotable, buf,
-                    sizeof ep->agent.mta.relay.helotable);
+	if (strcasecmp("mta-relay-helo", field) == 0)
+		return envelope_ascii_load_string(ep->agent.mta.relay.helotable, buf,
+		    sizeof ep->agent.mta.relay.helotable);
 
-	if (strcasecmp("mta-relay", field)  == 0)
-                return envelope_ascii_load_mta_relay_url_v1(&ep->agent.mta.relay, buf);
+	if (strcasecmp("mta-relay", field) == 0)
+		return envelope_ascii_load_mta_relay_url_v1(&ep->agent.mta.relay, buf);
 
-	if (strcasecmp("ctime", field)  == 0)
-                return envelope_ascii_load_time(&ep->creation, buf);
+	if (strcasecmp("ctime", field) == 0)
+		return envelope_ascii_load_time(&ep->creation, buf);
 
-	if (strcasecmp("expire", field)  == 0)
-                return envelope_ascii_load_time(&ep->expire, buf);
+	if (strcasecmp("expire", field) == 0)
+		return envelope_ascii_load_time(&ep->expire, buf);
 
-	if (strcasecmp("retry", field)  == 0)
-                return envelope_ascii_load_uint16(&ep->retry, buf);
+	if (strcasecmp("retry", field) == 0)
+		return envelope_ascii_load_uint16(&ep->retry, buf);
 
-	if (strcasecmp("last-try", field)  == 0)
-                return envelope_ascii_load_time(&ep->lasttry, buf);
+	if (strcasecmp("last-try", field) == 0)
+		return envelope_ascii_load_time(&ep->lasttry, buf);
 
-	if (strcasecmp("last-bounce", field)  == 0)
-                return envelope_ascii_load_time(&ep->lastbounce, buf);
+	if (strcasecmp("last-bounce", field) == 0)
+		return envelope_ascii_load_time(&ep->lastbounce, buf);
 
-	if (strcasecmp("flags", field)  == 0)
-                return envelope_ascii_load_flags(&ep->flags, buf);
+	if (strcasecmp("flags", field) == 0)
+		return envelope_ascii_load_flags(&ep->flags, buf);
 
-	if (strcasecmp("bounce-type", field)  == 0)
-                return envelope_ascii_load_bounce_type(&ep->agent.bounce.type, buf);
+	if (strcasecmp("bounce-type", field) == 0)
+		return envelope_ascii_load_bounce_type(&ep->agent.bounce.type, buf);
 
-	if (strcasecmp("bounce-delay", field)  == 0)
-                return envelope_ascii_load_time(&ep->agent.bounce.delay, buf);
+	if (strcasecmp("bounce-delay", field) == 0)
+		return envelope_ascii_load_time(&ep->agent.bounce.delay, buf);
 
-	if (strcasecmp("bounce-expire", field)  == 0)
-                return envelope_ascii_load_time(&ep->agent.bounce.expire, buf);
+	if (strcasecmp("bounce-expire", field) == 0)
+		return envelope_ascii_load_time(&ep->agent.bounce.expire, buf);
 
 	return 0;
 }
