1d1aecb048bd9024b2918511915a98c8f3be65d7
Charles Longeau <github@chl.be>
2013-11-02 17:46:16 +0100

    fix format string warnings

--- smtpd/filter_api.c
+++ smtpd/filter_api.c
@@ -676,11 +676,11 @@ filter_dispatch_dataline(uint64_t id, const char *data)
 static void
 filter_trigger_eom(struct filter_session *s)
 {
-	log_debug("debug: filter-api:%s: tx eom (%zu) for %p", filter_name, s->pipe.datalen, s->id);
+	log_debug("debug: filter-api:%s: tx eom (%zu) for %016"PRIx64, filter_name, s->pipe.datalen, s->id);
 
 	if (!s->pipe.error && s->pipe.idatalen != s->pipe.datalen) {
 		log_debug("debug: filter-api:%s: tx datalen mismatch: %zu/%zu",
-		    filter_name, s->pipe.idatalen != s->pipe.datalen);
+		    filter_name, s->pipe.idatalen, s->pipe.datalen);
 		s->pipe.error = 1;
 	}
 	if (s->pipe.error) {
