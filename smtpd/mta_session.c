0bc066cbf0ccbe705356068fb3762feb8e793a12
Gilles Chehade <gilles@poolp.org>
2014-03-02 23:22:06 +0100

    do not add trailing spaces when sending MAIL/RCPT commands if peer advertises DSN and we don't want it

--- smtpd/mta_session.c
+++ smtpd/mta_session.c
@@ -773,11 +773,11 @@ mta_enter_state(struct mta_session *s, int newstate)
 		s->msgtried++;
 		envid_sz = strlen(e->dsn_envid);
 		if (s->ext & MTA_EXT_DSN) {
-			mta_send(s, "MAIL FROM:<%s> %s%s %s%s",
+			mta_send(s, "MAIL FROM:<%s>%s%s%s%s",
 			    s->task->sender,
-			    e->dsn_ret ? "RET=" : "",
+			    e->dsn_ret ? " RET=" : "",
 			    e->dsn_ret ? dsn_strret(e->dsn_ret) : "",
-			    envid_sz ? "ENVID=" : "",
+			    envid_sz ? " ENVID=" : "",
 			    envid_sz ? e->dsn_envid : "");
 		} else
 			mta_send(s, "MAIL FROM:<%s>", s->task->sender);
@@ -789,11 +789,11 @@ mta_enter_state(struct mta_session *s, int newstate)
 
 		e = s->currevp;
 		if (s->ext & MTA_EXT_DSN) {
-			mta_send(s, "RCPT TO:<%s> %s%s %s%s",
+			mta_send(s, "RCPT TO:<%s>%s%s%s%s",
 			    e->dest,
-			    e->dsn_notify ? "NOTIFY=" : "",
+			    e->dsn_notify ? " NOTIFY=" : "",
 			    e->dsn_notify ? dsn_strnotify(e->dsn_notify) : "",
-			    e->dsn_orcpt ? "ORCPT=" : "",
+			    e->dsn_orcpt ? " ORCPT=" : "",
 			    e->dsn_orcpt ? e->dsn_orcpt : "");
 		} else
 			mta_send(s, "RCPT TO:<%s>", e->dest);
