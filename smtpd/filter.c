ee723ab2decbaa70c079b7ed2b883dd47b832f9e
Gilles Chehade <gilles@poolp.org>
2014-06-17 18:57:56 +0200

    eric has made smtpd way too chatty, log filters codepath using the log_trace facility, not log_debug

--- smtpd/filter.c
+++ smtpd/filter.c
@@ -374,7 +374,7 @@ filter_set_sink(struct filter_session *s, int sink)
 
 	while(s->fcurr) {
 		if (s->fcurr->proc->hooks & HOOK_DATALINE) {
-			log_trace(TRACE_MFA, "filter: sending fd %d to %s", sink, filter_to_text(s->fcurr));
+			log_trace(TRACE_FILTERS, "filter: sending fd %d to %s", sink, filter_to_text(s->fcurr));
 			p = &s->fcurr->proc->mproc;
 			m_create(p, IMSG_FILTER_PIPE, 0, 0, sink);
 			m_add_id(p, s->id);
@@ -384,7 +384,7 @@ filter_set_sink(struct filter_session *s, int sink)
 		s->fcurr = TAILQ_PREV(s->fcurr, filter_lst, entry);
 	}
 
-	log_trace(TRACE_MFA, "filter: chain input is %d", sink);
+	log_trace(TRACE_FILTERS, "filter: chain input is %d", sink);
 
 	smtp_filter_fd(s->id, sink);
 }
@@ -419,10 +419,10 @@ filter_query(struct filter_session *s, int kind, int type)
 	q->hasrun = 0;
 
 	if (kind == QK_QUERY)
-		log_trace(TRACE_MFA, "filter: new query %s %s", kind_to_str(kind),
+		log_trace(TRACE_FILTERS, "filter: new query %s %s", kind_to_str(kind),
 		    query_to_str(type));
 	else
-		log_trace(TRACE_MFA, "filter: new query %s %s", kind_to_str(kind),
+		log_trace(TRACE_FILTERS, "filter: new query %s %s", kind_to_str(kind),
 		    event_to_str(type));
 
 	return (q);
@@ -433,7 +433,7 @@ filter_drain_query(struct filter_query *q)
 {
 	struct filter_query	*prev;
 
-	log_trace(TRACE_MFA, "filter: filter_drain_query %s", filter_query_to_text(q));
+	log_trace(TRACE_FILTERS, "filter: filter_drain_query %s", filter_query_to_text(q));
 
 	/*
 	 * The query must be passed through all filters that registered
@@ -450,7 +450,7 @@ filter_drain_query(struct filter_query *q)
 				q->hasrun = 1;
 			}
 			if (q->state == QUERY_RUNNING) {
-				log_trace(TRACE_MFA,
+				log_trace(TRACE_FILTERS,
 				    "filter: waiting for running query %s",
 				    filter_query_to_text(q));
 				return;
@@ -463,7 +463,7 @@ filter_drain_query(struct filter_query *q)
 			prev = TAILQ_PREV(q, filter_query_lst, entry);
 			if (prev && prev->current == q->current) {
 				q->state = QUERY_WAITING;
-				log_trace(TRACE_MFA,
+				log_trace(TRACE_FILTERS,
 				    "filter: query blocked by previous query %s",
 				    filter_query_to_text(prev));
 				return;
@@ -490,7 +490,7 @@ filter_run_query(struct filter *f, struct filter_query *q)
 {
 	if (q->kind == QK_QUERY) {
 
-		log_trace(TRACE_MFA, "filter: running filter %s for query %s",
+		log_trace(TRACE_FILTERS, "filter: running filter %s for query %s",
 		    filter_to_text(f), filter_query_to_text(q));
 
 		m_create(&f->proc->mproc, IMSG_FILTER_QUERY, 0, 0, -1);
@@ -525,7 +525,7 @@ filter_run_query(struct filter *f, struct filter_query *q)
 		q->state = QUERY_RUNNING;
 	}
 	else {
-		log_trace(TRACE_MFA, "filter: running filter %s for query %s",
+		log_trace(TRACE_FILTERS, "filter: running filter %s for query %s",
 		    filter_to_text(f), filter_query_to_text(q));
 
 		m_create(&f->proc->mproc, IMSG_FILTER_EVENT, 0, 0, -1);
@@ -540,7 +540,7 @@ filter_end_query(struct filter_query *q)
 {
 	struct filter_session *s = q->session;
 
-	log_trace(TRACE_MFA, "filter: filter_end_query %s", filter_query_to_text(q));
+	log_trace(TRACE_FILTERS, "filter: filter_end_query %s", filter_query_to_text(q));
 
 	if (q->kind == QK_EVENT)
 		goto done;
@@ -564,7 +564,7 @@ filter_end_query(struct filter_query *q)
 		}
 	}
 
-	log_trace(TRACE_MFA,
+	log_trace(TRACE_FILTERS,
 	    "filter: query %016"PRIx64" done: "
 	    "status=%s code=%d response=\"%s\"",
 	    q->qid,
@@ -597,7 +597,7 @@ filter_imsg(struct mproc *p, struct imsg *imsg)
 		fatalx("exiting");
 	}
 
-	log_trace(TRACE_MFA, "filter: imsg %s from procfilter %s",
+	log_trace(TRACE_FILTERS, "filter: imsg %s from procfilter %s",
 	    filterimsg_to_str(imsg->hdr.type),
 	    filter_proc_to_text(proc));
 
