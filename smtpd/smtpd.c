9f72c49add3962656b84107d2afc5219ea35e606
Charles Longeau <github@chl.be>
2014-06-22 11:37:06 +0200

    fix cut/paste error in parent_broadcast_profile() to properly send IMSG_CTL_PROFILE to CA process

--- smtpd/smtpd.c
+++ smtpd/smtpd.c
@@ -1567,7 +1567,7 @@ parent_broadcast_profile(uint32_t v)
 	m_add_int(p_queue, v);
 	m_close(p_queue);
 
-	m_create(p_ca, IMSG_CTL_VERBOSE, 0, 0, -1);
+	m_create(p_ca, IMSG_CTL_PROFILE, 0, 0, -1);
 	m_add_int(p_ca, v);
 	m_close(p_ca);
 }
