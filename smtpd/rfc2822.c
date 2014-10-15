432ebb7d6a3f3e693ff1658a03a57580d8de5799
Gilles Chehade <gilles@poolp.org>
2014-10-15 21:10:59 +0200

    revert, incorrect

--- smtpd/rfc2822.c
+++ smtpd/rfc2822.c
@@ -76,7 +76,7 @@ parser_feed_header(struct rfc2822_parser *rp, char *line)
 	char			*pos;
 
 	/* new header */	
-	if (! isspace(*line) && *line != '\0') {
+	if (! isspace(*line)) {
 		rp->in_hdr = 1;
 		if ((pos = strchr(line, ':')) == NULL)
 			return 0;
