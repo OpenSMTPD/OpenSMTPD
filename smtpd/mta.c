f07931a2ca3558697159d2568e374be8cc450830
Gilles Chehade <gilles@poolp.org>
2015-02-08 11:31:23 +0100

    fix possible NULL deref
    spotted by Denis Denisov

--- smtpd/mta.c
+++ smtpd/mta.c
@@ -232,6 +232,7 @@ mta_imsg(struct mproc *p, struct imsg *imsg)
 				return;
 			}
 
+			task = NULL;
 			TAILQ_FOREACH(task, &relay->tasks, entry)
 				if (task->msgid == evpid_to_msgid(evp.id))
 					break;
