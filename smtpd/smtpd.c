7ed2b425b340bc4b291bb93f9962633c526930de
Gilles Chehade <gilles@rentabiliweb.com>
2015-10-01 00:14:14 +0200

    do not let user play symlink/hardlink/mkfifo games in offline enqueue

--- smtpd/smtpd.c
+++ smtpd/smtpd.c
@@ -1148,28 +1148,23 @@ offline_enqueue(char *name)
 
 	if (pid == 0) {
 		char	*envp[2], *p, *tmp;
+		int	 fd;
 		FILE	*fp;
 		size_t	 len;
 		arglist	 args;
 
+		if (closefrom(STDERR_FILENO + 1) == -1)
+			_exit(1);
+
 		memset(&args, 0, sizeof(args));
 
-		if (lstat(path, &sb) == -1) {
-			log_warn("warn: smtpd: lstat: %s", path);
+		if ((fd = open(path, O_RDONLY|O_NOFOLLOW|O_NONBLOCK)) == -1) {
+			log_warn("warn: smtpd: open: %s", path);
 			_exit(1);
 		}
 
-#ifdef HAVE_CHFLAGS
-		if (chflags(path, 0) == -1) {
-			log_warn("warn: smtpd: chflags: %s", path);
-			_exit(1);
-		}
-#endif
-
-		pw = getpwuid(sb.st_uid);
-		if (pw == NULL) {
-			log_warnx("warn: smtpd: getpwuid for uid %d failed",
-			    sb.st_uid);
+		if (fstat(fd, &sb) == -1) {
+			log_warn("warn: smtpd: fstat: %s", path);
 			_exit(1);
 		}
 
@@ -1179,14 +1174,35 @@ offline_enqueue(char *name)
 			_exit(1);
 		}
 
+		if (sb.st_nlink != 1) {
+			log_warnx("warn: smtpd: file %s is hard-link", path);
+			_exit(1);
+		}
+
+		pw = getpwuid(sb.st_uid);
+		if (pw == NULL) {
+			log_warnx("warn: smtpd: getpwuid for uid %d failed",
+			    sb.st_uid);
+			_exit(1);
+		}
+
+		if (fchflags(fd, 0) == -1) {
+			log_warn("warn: smtpd: chflags: %s", path);
+			_exit(1);
+		}
+
 		if (setgroups(1, &pw->pw_gid) ||
 		    setresgid(pw->pw_gid, pw->pw_gid, pw->pw_gid) ||
 		    setresuid(pw->pw_uid, pw->pw_uid, pw->pw_uid))
 			_exit(1);
 
+<<<<<<< HEAD
 		closefrom(STDERR_FILENO + 1);
 
 		if ((fp = fopen(path, "r")) == NULL)
+=======
+		if ((fp = fdopen(fd, "r")) == NULL)
+>>>>>>> 08793c7... do not let user play symlink/hardlink/mkfifo games in offline enqueue
 			_exit(1);
 
 		if (chdir(pw->pw_dir) == -1 && chdir("/") == -1)
