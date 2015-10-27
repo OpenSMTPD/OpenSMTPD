3632b12a92a0dcdd3021365b8dec2eefb955087a
Gilles Chehade <gilles@poolp.org>
2015-10-27 22:35:20 +0100

    sync

--- smtpd/delivery_lmtp.c
+++ smtpd/delivery_lmtp.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: delivery_lmtp.c,v 1.12 2015/10/19 09:15:35 sunil Exp $ */
+/* $OpenBSD: delivery_lmtp.c,v 1.14 2015/10/27 21:20:11 jung Exp $ */
 
 /*
  * Copyright (c) 2013 Ashish SHUKLA <ashish.is@lostca.se>
@@ -119,16 +119,18 @@ static void
 lmtp_open(struct deliver *deliver)
 {
 	FILE		*fp;
-	char		*buf = NULL, hn[HOST_NAME_MAX+1], *to = deliver->to;
+	char		*buf = NULL, hn[HOST_NAME_MAX + 1],
+			*rcpt = deliver->to, *to = deliver->to;
 	size_t		 sz = 0;
 	ssize_t		 len;
 	int		 s;
 
+	strsep(&rcpt, " "); 
 	s = (to[0] == '/') ? unix_socket(to) : inet_socket(to);
 	if ((fp = fdopen(s, "r+")) == NULL)
 		err(1, "fdopen");
 
-	if ((len = getline(&buf, &sz, fp)) == -1)
+	if (getline(&buf, &sz, fp) == -1)
 		err(1, "getline");
 
 	if (buf[0] != '2')
@@ -143,16 +145,15 @@ lmtp_open(struct deliver *deliver)
 	if (lmtp_cmd(&buf, &sz, '2', fp, "MAIL FROM:<%s>", deliver->from) != 0)
 		errx(1, "MAIL FROM rejected: %s", buf);
 
-	if (lmtp_cmd(&buf, &sz, '2', fp, "RCPT TO:<%s>", deliver->user) != 0)
+	if (lmtp_cmd(&buf, &sz, '2', fp, "RCPT TO:<%s>",
+	    rcpt ? deliver->dest : deliver->user) != 0)
 		errx(1, "RCPT TO rejected: %s", buf);
 
 	if (lmtp_cmd(&buf, &sz, '3', fp, "DATA") != 0)
 		errx(1, "Invalid DATA reply: %s", buf);
 
 	while ((len = getline(&buf, &sz, stdin)) != -1) {
-		if (len >= 2 && buf[len - 2] == '\r')
-			buf[len - 2] = '\0';
-		else if (buf[len - 1] == '\n')
+		if (buf[len - 1] == '\n')
 			buf[len - 1] = '\0';
 
 		if (fprintf(fp, "%s%s\r\n", buf[0] == '.' ? "." : "", buf) < 0)
