d49761059a8d9483bb864b8e3c26a67f9f27fe25
Gilles Chehade <gilles@dalenys.com>
2016-02-14 12:21:43 +0100

    howmany() is unused in our compat layer AND opensmtpd code

--- openbsd-compat/defines.h
+++ openbsd-compat/defines.h
@@ -355,10 +355,6 @@ struct winsize {
 # define	NFDBITS (8 * sizeof(unsigned long))
 #endif
 
-#if defined(HAVE_DECL_HOWMANY) && HAVE_DECL_HOWMANY == 0
-# define howmany(x,y)	(((x)+((y)-1))/(y))
-#endif
-
 /* Paths */
 
 #ifndef _PATH_BSHELL
