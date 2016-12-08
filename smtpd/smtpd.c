0969e378029d155e3c01a073946f4876a704c816
Freddy DISSAUX <dsx@bsdsx.fr>
2016-12-08 22:09:04 +0100

    xclosefrom
    Use xclosefrom instead closefrom.
    On Solaris, smtpd/smtpd.c build fine.

--- smtpd/smtpd.c
+++ smtpd/smtpd.c
@@ -867,7 +867,7 @@ start_child(int save_argc, char **save_argv, char *rexec)
 	if (dup2(sp[0], 3) == -1)
 		fatal("%s: dup2", rexec);
 
-	if (closefrom(4) == -1)
+	if (xclosefrom(4) == -1)
 		fatal("%s: closefrom", rexec);
 
 	for (argc = 0; argc < save_argc; argc++)
