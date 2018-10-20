520e67ba57d3a6a383e8819baf341f39d1780c1e
Gilles Chehade <gilles@poolp.org>
2018-10-20 19:05:26 +0200

    free in error path

--- smtpd/config.c
+++ smtpd/config.c
@@ -152,6 +152,7 @@ error:
 	free(conf->sc_ssl_dict);
 	free(conf->sc_limits_dict);
 	free(conf->sc_mda_wrappers);
+	free(conf->sc_processors_dict);
 	free(conf->sc_dispatcher_bounce);
 	free(limits);
 	free(conf);
