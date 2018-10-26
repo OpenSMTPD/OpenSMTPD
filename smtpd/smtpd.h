6f922e396a293301d37132a71af17002ca7928ec
Gilles Chehade <gilles@poolp.org>
2018-10-26 15:26:01 +0200

    set version

--- smtpd/smtpd.h
+++ smtpd/smtpd.h
@@ -73,7 +73,7 @@
 #ifndef SMTPD_NAME
 #define	SMTPD_NAME		 "OpenSMTPD"
 #endif
-#define	SMTPD_VERSION		 "6.4.0-portable"
+#define	SMTPD_VERSION		 "6.4.0p1"
 #define SMTPD_SESSION_TIMEOUT	 300
 #define SMTPD_BACKLOG		 5
 
