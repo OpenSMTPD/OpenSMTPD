c9538fff028dd2e90d7097854dbf7c5ff47956c0
Gilles Chehade <gilles@poolp.org>
2019-11-17 12:32:44 +0100

    unbreak getopt() ftb on linux

--- openbsd-compat/defines.h
+++ openbsd-compat/defines.h
@@ -357,21 +357,6 @@ typedef uint16_t	in_port_t;
 
 /* Function replacement / compatibility hacks */
 
-#ifndef HAVE_GETOPT_OPTRESET
-# undef getopt
-# undef opterr
-# undef optind
-# undef optopt
-# undef optreset
-# undef optarg
-# define getopt(ac, av, o)  BSDgetopt(ac, av, o)
-# define opterr             BSDopterr
-# define optind             BSDoptind
-# define optopt             BSDoptopt
-# define optreset           BSDoptreset
-# define optarg             BSDoptarg
-#endif
-
 #if defined(BROKEN_GETADDRINFO) && defined(HAVE_GETADDRINFO)
 # undef HAVE_GETADDRINFO
 #endif
