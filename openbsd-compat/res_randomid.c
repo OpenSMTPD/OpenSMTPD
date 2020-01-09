2eab64d2b6d0d71a51fb63f9ebf1d92f035a2d8e
Gilles Chehade <gilles@poolp.org>
2020-01-09 02:14:22 +0100

    detect res_randomid()

---
+++ openbsd-compat/res_randomid.c
@@ -0,0 +1,11 @@
+#include "includes.h"
+
+unsigned int
+res_randomid(void)
+{
+	struct timespec ts;
+
+	/* This is from musl C library */
+	clock_gettime(CLOCK_MONOTONIC, &ts);
+	return ts.tv_nsec + ts.tv_nsec / 65536UL & 0xffff;
+}
