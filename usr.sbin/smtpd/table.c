6d1e617b1e3fd98ebf90dbd8c5cc83559b693ccf
Gilles CHEHADE <gilles@poolp.org>
2020-12-23 14:58:20 +0100

    plug a leak in regex table lookups

--- usr.sbin/smtpd/table.c
+++ usr.sbin/smtpd/table.c
@@ -470,6 +470,7 @@ table_regex_match(const char *string, const char *pattern)
 {
 	regex_t preg;
 	int	cflags = REG_EXTENDED|REG_NOSUB;
+	int	ret;
 
 	if (strncmp(pattern, "(?i)", 4) == 0) {
 		cflags |= REG_ICASE;
@@ -479,7 +480,11 @@ table_regex_match(const char *string, const char *pattern)
 	if (regcomp(&preg, pattern, cflags) != 0)
 		return (0);
 
-	if (regexec(&preg, string, 0, NULL, 0) != 0)
+	ret = regexec(&preg, string, 0, NULL, 0);
+
+	regfree(&preg);
+
+	if (ret != 0)
 		return (0);
 
 	return (1);
