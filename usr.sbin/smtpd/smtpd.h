ec399d75b1a4e4a93d07e2845a152a76d8044896
Gilles CHEHADE <gilles@poolp.org>
2020-12-28 22:04:24 +0100

    bump version

--- usr.sbin/smtpd/smtpd.h
+++ usr.sbin/smtpd/smtpd.h
@@ -73,7 +73,7 @@
 #ifndef SMTPD_NAME
 #define	SMTPD_NAME		 "OpenSMTPD"
 #endif
-#define	SMTPD_VERSION		 "6.7.0-portable"
+#define	SMTPD_VERSION		 "6.9.0-portable"
 #define SMTPD_SESSION_TIMEOUT	 300
 #define SMTPD_BACKLOG		 5
 
