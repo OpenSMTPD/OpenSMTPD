89de926d38808b131b47dee8673f69ef176c85b4
Omar Polo <op@omarpolo.com>
2023-09-14 22:04:56 +0000

    fmt

--- openbsd-compat/libtls/asn.c
+++ openbsd-compat/libtls/asn.c
@@ -26,53 +26,53 @@
 #define GENTIME_LENGTH 15
 #define UTCTIME_LENGTH 13
 
-#define V_ASN1_UTCTIME          23
-#define V_ASN1_GENERALIZEDTIME  24
+#define V_ASN1_UTCTIME		23
+#define V_ASN1_GENERALIZEDTIME	24
 
 int
 ASN1_time_tm_cmp(struct tm *tm1, struct tm *tm2)
 {
-        if (tm1->tm_year < tm2->tm_year)
-                return (-1);
-        if (tm1->tm_year > tm2->tm_year)
-                return (1);
-        if (tm1->tm_mon < tm2->tm_mon)
-                return (-1);
-        if (tm1->tm_mon > tm2->tm_mon)
-                return (1);
-        if (tm1->tm_mday < tm2->tm_mday)
-                return (-1);
-        if (tm1->tm_mday > tm2->tm_mday)
-                return (1);
-        if (tm1->tm_hour < tm2->tm_hour)
-                return (-1);
-        if (tm1->tm_hour > tm2->tm_hour)
-                return (1);
-        if (tm1->tm_min < tm2->tm_min)
-                return (-1);
-        if (tm1->tm_min > tm2->tm_min)
-                return (1);
-        if (tm1->tm_sec < tm2->tm_sec)
-                return (-1);
-        if (tm1->tm_sec > tm2->tm_sec)
-                return (1);
-        return 0;
+	if (tm1->tm_year < tm2->tm_year)
+		return (-1);
+	if (tm1->tm_year > tm2->tm_year)
+		return (1);
+	if (tm1->tm_mon < tm2->tm_mon)
+		return (-1);
+	if (tm1->tm_mon > tm2->tm_mon)
+		return (1);
+	if (tm1->tm_mday < tm2->tm_mday)
+		return (-1);
+	if (tm1->tm_mday > tm2->tm_mday)
+		return (1);
+	if (tm1->tm_hour < tm2->tm_hour)
+		return (-1);
+	if (tm1->tm_hour > tm2->tm_hour)
+		return (1);
+	if (tm1->tm_min < tm2->tm_min)
+		return (-1);
+	if (tm1->tm_min > tm2->tm_min)
+		return (1);
+	if (tm1->tm_sec < tm2->tm_sec)
+		return (-1);
+	if (tm1->tm_sec > tm2->tm_sec)
+		return (1);
+	return 0;
 }
 
 int
 ASN1_time_tm_clamp_notafter(struct tm *tm)
 {
 #ifdef SMALL_TIME_T
-        struct tm broken_os_epoch_tm;
-        time_t broken_os_epoch_time = INT_MAX;
+	struct tm broken_os_epoch_tm;
+	time_t broken_os_epoch_time = INT_MAX;
 
-        if (gmtime_r(&broken_os_epoch_time, &broken_os_epoch_tm) == NULL)
-                return 0;
+	if (gmtime_r(&broken_os_epoch_time, &broken_os_epoch_tm) == NULL)
+		return 0;
 
-        if (ASN1_time_tm_cmp(tm, &broken_os_epoch_tm) == 1)
-                memcpy(tm, &broken_os_epoch_tm, sizeof(*tm));
+	if (ASN1_time_tm_cmp(tm, &broken_os_epoch_tm) == 1)
+		memcpy(tm, &broken_os_epoch_tm, sizeof(*tm));
 #endif
-        return 1;
+	return 1;
 }
 
 /*
